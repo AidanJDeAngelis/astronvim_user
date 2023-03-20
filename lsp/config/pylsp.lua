b9ee020313b2a3f4b6ef578d9947240f19c26cee
Aidan De Angelis <angaidan@amazon.com>
2023-03-19 22:23:01 -0700

    Configure LSP settings

---
+++ lsp/config/pylsp.lua
@@ -0,0 +1,34 @@
+return {
+  settings = {
+    pylsp = {
+      configurationSources = { "flake8" },
+      plugins = {
+        flake8 = {
+          enabled = true,
+          maxComplexity = 15,
+          maxLineLength = 99,
+        },
+        jedi_completion = {
+          include_class_objects = true,
+          include_function_objects = true,
+          fuzzy = true,
+        },
+        mccabe = {
+          enabled = false,
+        },
+        pycodestyle = {
+          enabled = false,
+        },
+        pyflakes = {
+          enabled = false,
+        },
+        rope_completion = {
+          enabled = true,
+        },
+        yapf = {
+          enabled = false,
+        },
+      },
+    },
+  },
+}
