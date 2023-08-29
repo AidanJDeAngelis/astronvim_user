d42e049569e1e6406cf2f9b3475b4a5d4e27d96f
Aidan De Angelis <angaidan@amazon.com>
2023-08-29 14:18:51 -0700

    Enable resession for session management, autosave session on entrance and exit

--- options.lua
+++ options.lua
@@ -22,6 +22,7 @@ return function(local_vim)
   local_vim.g.diagnostics_mode = 3 -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
   local_vim.g.icons_enabled = true -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
   local_vim.g.ui_notifications_enabled = true -- disable notifications when toggling UI elements
+  local_vim.g.resession_enabled = true -- experimental resession support
 
   -- Neovide Configuration
   if vim.g.neovide then
--- polish.lua
+++ polish.lua
@@ -1,6 +1,52 @@
 -- This function is run last and is a good place to configuring
 -- augroups/autocommands and custom filetypes also this just pure lua so
 -- anything that doesn't fit in the normal config locations above can go here
+local resession_autocmds = function()
+  local resession = require "resession"
+  local function get_session_name()
+    local name = vim.fn.getcwd()
+    local branch = vim.trim(vim.fn.system "git branch --show-current")
+    if vim.v.shell_error == 0 then
+      return name .. branch
+    else
+      return name
+    end
+  end
+  vim.api.nvim_create_autocmd("VimEnter", {
+    callback = function()
+      -- If args are passed to nvim then skip prompt
+      if vim.fn.argc(-1) ~= 0 then return end
+      -- Else, prompt
+      local resume_session = "Resume Previous Session"
+      local new_session = "New Session"
+      local quit = "Quit"
+
+      vim.ui.select({ resume_session, new_session, quit }, {
+        prompt = "What It Do?",
+        telescope = require("telescope.themes").get_dropdown(),
+      }, function(selected)
+        if selected == resume_session then
+          resession.load(get_session_name(), { dir = "dirsession", silence_errors = true })
+        elseif selected == "New Session" then
+          require("telescope.builtin").builtin { include_extensions = true }
+        elseif selected == quit then
+          -- Load last session before quitting to prevent it from being lost
+          resession.load(get_session_name(), { dir = "dirsession", silence_errors = true })
+          vim.cmd "qa!"
+        end
+      end)
+    end,
+  })
+  vim.api.nvim_create_autocmd("VimLeavePre", {
+    callback = function() resession.save(get_session_name(), { dir = "dirsession", notify = false }) end,
+  })
+end
+
+local override_colors = function()
+  vim.cmd "hi LineNr guibg=none guifg=#ffffff"
+  vim.cmd "hi WinSeparator guibg=none guifg=#ffffff"
+end
+
 return function()
   -- Set up custom filetypes
   -- vim.filetype.add {
@@ -14,6 +60,6 @@ return function()
   --     ["~/%.config/foo/.*"] = "fooscript",
   --   },
   -- }
-  vim.cmd "hi LineNr guibg=none guifg=#ffffff"
-  vim.cmd "hi WinSeparator guibg=none guifg=#ffffff"
+  resession_autocmds()
+  override_colors()
 end
