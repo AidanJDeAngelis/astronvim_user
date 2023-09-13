deeb7a1aa87d8978985c4ef58f9107172bdbbf53
Aidan De Angelis <angaidan@amazon.com>
2023-09-13 15:45:32 -0700

    Remap Codeium to stop arrow key hijack

--- plugins/user.lua
+++ plugins/user.lua
@@ -15,16 +15,16 @@ return {
     event = "User AstroFile",
     config = function()
       vim.keymap.set("i", "<C-g>", function() return vim.fn["codeium#Accept"]() end, { expr = true })
-      vim.keymap.set("i", "<Right>", function() return vim.fn["codeium#Accept"]() end, { expr = true })
+      vim.keymap.set("i", "<S-Right>", function() return vim.fn["codeium#Accept"]() end, { expr = true })
 
       vim.keymap.set("i", "<c-;>", function() return vim.fn["codeium#CycleCompletions"](1) end, { expr = true })
-      vim.keymap.set("i", "<Down>", function() return vim.fn["codeium#CycleCompletions"](1) end, { expr = true })
+      vim.keymap.set("i", "<S-Down>", function() return vim.fn["codeium#CycleCompletions"](1) end, { expr = true })
 
       vim.keymap.set("i", "<c-,>", function() return vim.fn["codeium#CycleCompletions"](-1) end, { expr = true })
-      vim.keymap.set("i", "<Up>", function() return vim.fn["codeium#CycleCompletions"](-1) end, { expr = true })
+      vim.keymap.set("i", "<S-Up>", function() return vim.fn["codeium#CycleCompletions"](-1) end, { expr = true })
 
       vim.keymap.set("i", "<c-x>", function() return vim.fn["codeium#Clear"]() end, { expr = true })
-      vim.keymap.set("i", "<Left>", function() return vim.fn["codeium#Clear"]() end, { expr = true })
+      vim.keymap.set("i", "<S-Left>", function() return vim.fn["codeium#Clear"]() end, { expr = true })
 
       vim.keymap.set("n", "<leader>;", function()
         if vim.g.codeium_enabled == true then
